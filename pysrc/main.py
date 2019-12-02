bf4765660fa1ed618ca2ad8f0daccd8a30e0f502
Mike Lippert <mlippert255@gmail.com>
2019-12-02 16:10:18 -0500

    complete move of visualization code to visualize package
    no lint errors

--- pysrc/main.py
+++ pysrc/main.py
@@ -1,26 +1,27 @@
-#################################################################################
-# main.py                                                                       #
-#################################################################################
-#
-# This is the main module to provide context for all the local application pkgs
-#
-# Created on    December 2, 2019
-# author        Michael Jay Lippert
-#
-# (c) 2019-present Riff Learning Inc.,
-# MIT License (see https://opensource.org/licenses/MIT)
-#
-#################################################################################
+"""
+################################################################################
+  main.py
+################################################################################
+
+This is the main module to provide context for all the local application pkgs
+
+=============== ================================================================
+Created on      December 2, 2019
+--------------- ----------------------------------------------------------------
+author(s)       Michael Jay Lippert
+--------------- ----------------------------------------------------------------
+Copyright       (c) 2019-present Riff Learning Inc.,
+                MIT License (see https://opensource.org/licenses/MIT)
+=============== ================================================================
+"""
 
 # Standard library imports
-from datetime import timedelta
 
 # Third party imports
-import matplotlib.pyplot as plt
-import numpy as np
 
 # Local application imports
 from visualize.utterance_duration import do_analysis
+
 
 def _test():
     do_analysis()
--- pysrc/riffdata/riffdata.py
+++ pysrc/riffdata/riffdata.py
@@ -1,22 +1,28 @@
-#################################################################################
-# riffdata.py                                                                   #
-#################################################################################
-#
-# The riffdata module retrieves the raw riffdata from the database
-#
-# Created on    November 27, 2019
-# author        Michael Jay Lippert
-#
-# (c) 2019-present Riff Learning Inc.,
-# MIT License (see https://opensource.org/licenses/MIT)
-#
-#################################################################################
+"""
+################################################################################
+  riffdata.riffdata.py
+################################################################################
 
+The riffdata module retrieves the raw riffdata from the database
+
+=============== ================================================================
+Created on      November 27, 2019
+--------------- ----------------------------------------------------------------
+author(s)       Michael Jay Lippert
+--------------- ----------------------------------------------------------------
+Copyright       (c) 2019-present Riff Learning Inc.,
+                MIT License (see https://opensource.org/licenses/MIT)
+=============== ================================================================
+"""
+
+# Standard library imports
 import pprint
 from datetime import timedelta
+
+# Third party imports
 from pymongo import MongoClient
-import matplotlib.pyplot as plt
-import numpy as np
+
+# Local application imports
 
 dbDomain = 'localhost'
 dbPort = 27017
@@ -131,117 +137,8 @@ def get_meetings_with_participant_utterances():
     return meetings
 
 
-# Just for expediency for now, everything is going in this file/module
-# but this analysis really should go elsewhere eventually
-
-def get_utterance_durations(meetings):
-    """
-    Get a list of the duration of all utterances from all meetings
-    """
-    durations = []
-
-    for meeting_id in meetings:
-        participant_uts = meetings[meeting_id]
-        for participant_id in participant_uts:
-            uts = participant_uts[participant_id]
-            durations += [ut['duration'] for ut in uts]
-
-    return durations
-
-
-def my_plotter(ax, data1, data2, param_dict):
-    """
-    A helper function to make a graph
-
-    Parameters
-    ----------
-    ax : Axes
-        The axes to draw to
-
-    data1 : array
-       The x data
-
-    data2 : array
-       The y data
-
-    param_dict : dict
-       Dictionary of kwargs to pass to ax.plot
-
-    Returns
-    -------
-    out : list
-        list of artists added
-    """
-    out = ax.plot(data1, data2, **param_dict)
-    return out
-
-
 def _test():
-    meetings = get_meetings_with_participant_utterances()
-
-    print(f'Found utterances from {len(meetings)} meetings')
-    meeting_ids = list(meetings)
-
-    participant_uts = meetings[meeting_ids[0]]
-    print(f'For the meeting with id {meeting_ids[0]}:')
-    for participant_id in participant_uts:
-        print(f'  participant id {participant_id} had {len(participant_uts[participant_id])} utterances')
-
-    durations = get_utterance_durations(meetings)
-
-    print(f'Found {len(durations)} utterances')
-
-    durations.sort()
-
-    print(f'shortest utterance was {durations[0]}ms and longest was {durations[-1]}ms')
-
-    buckets = [0, 2, 5,                         #   0 -   2
-               *range(10, 300, 10),             #   3 -  32
-               *range(300, 3500, 50),           #  33 -  96
-               *range(3500, 8001, 500),         #  97 - 106
-               *range(10000, 60001, 10000),     # 107 - 113
-              ]
-
-    bucket_cnt = [0]
-    bucket_ndx = 0
-    for duration in durations:
-        if bucket_ndx >= len(buckets) or duration <= buckets[bucket_ndx]:
-            bucket_cnt[bucket_ndx] += 1
-            continue
-
-        bucket_ndx += 1
-        while bucket_ndx < len(buckets) and duration > buckets[bucket_ndx]:
-            bucket_cnt.append(0)
-            bucket_ndx += 1
-
-        bucket_cnt.append(1)
-
-    for i in range(0, len(bucket_cnt) - 1):
-        print(f'{timedelta(milliseconds=buckets[i])}: {bucket_cnt[i]}')
-
-    print(f'>: {bucket_cnt[len(bucket_cnt) - 1]}')
-
-    graph_ranges = [1, 33, 97, 107]
-
-    def _make_xy_sets_to_plot(x_src, y_src, ranges):
-        # must have at least 2 indices in ranges, not checking for that yet
-        x = []
-        y = []
-        start = ranges[0]
-        for end in ranges[1:]:
-            x.append(np.array(x_src[start:end]))
-            y.append(np.array(y_src[start:end]))
-            start = end
-
-        return x, y
-
-    x, y = _make_xy_sets_to_plot(x_src=buckets, y_src=bucket_cnt, ranges=graph_ranges)
-
-    fig, ax = plt.subplots(len(x), 1)
-    for plot in range(0, len(x)):
-        my_plotter(ax[plot], x[plot], y[plot], {'marker': 'x'})
-
-    fig.savefig('plot.png')
+    pass
 
 
 if __name__ == '__main__':
